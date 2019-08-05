7082c8826161ab5bcd1f2f6e67011a43b5b0c151
TermuxIna <50316075+TermuxIna@users.noreply.github.com>
2019-08-06 00:22:03 +0700

    Create newbie2.sh

---
+++ newbie2.sh
@@ -0,0 +1,472 @@
+#!/bin/bash
+#RecodeGakAkanBuatLuPro
+#SavePalestina
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pu='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+clear
+echo "卐卐卐卐卐卐卐卐   |/=\|   卐
+              卐   |/=\|   卐
+              卐   |/=\|   卐
+              卐   |/=\|   卐
+              卐   |/=\|   卐
+ 卐卐卐卐卐卐卐卐卐卐卐卐卐卐
+ 卐   |/=\|   卐
+ 卐   |/=\|   卐
+ 卐   |/=\|   卐
+ 卐   |/=\|   卐
+ 卐   |/=\|   卐卐卐卐卐卐卐卐" |lolcat -a -d 10
+figlet -f slant "newbie2" |lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+echo "Name    : TheKingTermux" |lolcat -a -d 10
+echo "Github  : TermuxIna" |lolcat -a -d 10
+echo "Wa      : 087705736634" |lolcat -a -d 10
+echo "Fb      : TheKingg Termux" |lolcat -a -d 10
+echo "Ig      : thekingtermux" |lolcat -a -d 10
+echo "Yt      : Cari Sendiri " |lolcat -a -d 10
+echo "Rumah   : Cari Sendiri Jng Tanya" |lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+echo 'KUMPULAN SCRIPT NEWBIE2'|lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+echo 'MOHON MAAFKAN BILA ADA SCRIPT YG TIDAK BERFUNGSI DENGAN BAIK'|lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+echo 'HARAP DIGUNAKAN DENGAN BIJAK'|lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+echo 'VERSI KE 3 AKAN DIRILIS SETELAH VERSI 2 DIRILIS :V'|lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+echo 'UNTUK MEMPERLANCAR JALANNYA SCRIPT INI , SILAHKAN PILIH 99 DLU YAA ;-) .' |lolcat -a -d 10
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" |lolcat -a -d 10
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $me"[+]> (Ctrl + C ) Detected Trying To Exit ... " |lolcat -a -d 10
+echo $cy"[+]> Thanks For Using My Script" |lolcat -a -d 10
+sleep 1
+echo 'Thanks To : ALLAH SWT.' |lolcat -a -d 10
+echo 'Jangan lupa mampir lagi ya ke script ini' |lolcat -a -d 10
+echo ""
+echo $pu"[+]> see you gaes :)..." |lolcat -a -d 10
+sleep 1
+exit
+}
+
+echo $me"1.REPORT FB"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"2.BRUTE PORCE"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"3.SPAM SMS"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"4.PHISING"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"5.DEFACE"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"6.WebDav"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"7.Foto Profile GUARD FB"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $me"8.Hack-Wifi"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"9.Script-Deface"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"10.5Tools"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"11.OSIF"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"12.Yahoo Clone"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"13.Hack Fb Massal[MBF]"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"14.Lazymux"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $me"15.Nmap"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"16.TextToBinary"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"17.Tools Mr.Cakil"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"18.LazyGo-v1"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"19.ACN"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"20.IslamPray"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"21.KALSEL-Z-TOOL"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $me"22.Hack-CCTV" $cy"Not Work"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ku"23.DDos"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"24.TOOLSINSTALLERv1" $cy"100+ Tools"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"25.TOOLAINSTALLERv2" $bi"200+ Tools"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"26.TOOLSINSTALLERv3" $pu"300+ Tools"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $ij"27.TOOLSINSTALLERv4" $cy"400+ Tools"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"28.Perkiraan Cuaca"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $cy"29.ShortLink"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $bi"30.Dorking"
+echo "••••••••••••••••••••lolcat ••••••" | lolcat
+echo $ij"99.Install Bahan"
+echo "••••••••••••••••••••••••••••••••" | lolcat
+echo $pu"00.Exit"
+echo "╭─(pilih nomer nya mamank)" |lolcat
+read -p"╰─#" kaex
+
+if [ $kaex = 1 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $kaex = 2 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $kaex = 3 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/SPAM
+cd SPAM
+sh SPAM.sh
+fi
+
+if [ $kaex = 4 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/PHISING
+cd PHISING
+sh Phis.sh
+fi
+
+if [ $kaex = 5 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/DEFACE
+cd DEFACE
+sh DFC.sh
+fi
+
+if [ $kaex = 6 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/WebDav
+cd WebDav
+sh WebDav.sh
+fi
+
+if [ $kaex = 7 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/kumpulanremaja/pp-fb
+cd pp-fb
+php fb.php
+fi
+
+if [ $kaex = 8 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/Hack-Wifi
+cd Hack-Wifi
+sh Wifi.sh
+fi
+
+if [ $kaex = 9 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/Script-Deface
+cd Script-Deface
+python2 script-deface.py
+fi
+
+if [ $kaex = 10 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/Moreno-ID/5TOOLS
+cd 5TOOLS
+sh 5tools.sh
+fi
+
+if [ $kaex = 11 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF
+cd OSIF
+python2 osif.py
+fi
+
+if [ $kaex = 12 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python2 cloning.py
+fi
+
+if [ $kaex = 13 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/pirmansx/MBF
+cd MBF
+python2 MBF.py
+fi
+
+if [ $kaex = 14 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+https://github.com/Gameye98/Lazymux
+cd Lazymux
+python lazymux.py
+fi
+
+if [ $kaex = 15 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+pkg install nmap
+echo -e  "${y} {1} Masukkan Web${endc}:"
+read web
+nmap $web
+echo
+fi
+
+if [ $kaex = 16 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/FajarTheGGman/TextToBinary
+mv TextToBinary $HOME
+cd $HOME/TextToBinary
+sh install
+fi
+
+if [ $kaex = 17 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/mrcakil/Mrcakil
+cd Mrcakil
+./tools
+fi
+
+if [ $kaex = 18 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/kadal15/LazyGo-v1
+cd LazyGo-v1
+sh lazygo
+fi
+
+if [ $kaex = 19 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/MaulanaRyM/ACN                                                      cd ACN
+sh ACN.sh
+fi
+
+if [ $kaex = 20 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/FajarTheGGman/IslamPray
+cd IslamPray
+php islam.php
+fi
+
+if [ $kaex = 21 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/KALSELeXploit/KALSEL-Z-TOOL
+cd KALSEL-Z-TOOL
+sh Z-TOOL.sh
+fi
+
+if [ $kaex = 22 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $kaex = 23 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hyammer.py
+fi
+
+if [ $kaex = 24 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+sh Tuanb4dut.sh
+fi
+
+if [ $kaex = 25 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv2
+cd TOOLSINSTALLERv2
+sh Tuanb4dut.sh
+fi
+
+if [ $kaex = 26 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sh TUANB4DUT.sh
+fi
+
+if [ $kaex = 27 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+chmod +x TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+if [ $kaex = 28 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1.8
+git clone https://github.com/TUANB4DUT/PERKIRAAN-CUACA
+cd PERKIRAAN-CUACA
+sh CUACA.sh
+fi
+
+if [ $kaex = 29 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1.8
+git clone https://github.com/blackcodercrush/shortlink
+cd shortlink
+python2 shortlink.py
+fi
+
+if [ $kaex = 30 ]
+then
+clear
+figlet -f slant "SABAR WOY"|lolcat
+sleep 1.8
+git clone https://github.com/black-ware/Dorking
+cd Dorking
+sh Dork.sh
+fi
+
+if [ $kaex = 99 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install git
+pkg install moon-buggy
+pkg install sl
+pkg install neofetch
+pkg install python2 && pkg install python && pkg install python3
+pkg install proot
+pip2 install lolcat
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+pip2 install requests
+pip2 install mechanize
+pip2 install dirhunt
+pip2 install youtube-dl
+pkg install curl
+pkg install php
+pip2 install termcolor
+pip2 install bs4
+pip2 install beautifulsoup
+pip2 install colorama
+pkg install perl
+pkg install ruby
+pip install requests
+pkg install figlet
+fi
+
+if [ $kaex = 00 ]
+if [ $kaex = 0 ]
+then   
+figlet -f slant " E X I T " |lolcat -a -d 10                                                                    echo $me"TERIMA KASIH TELAH MENGGUNAKAN TOOL INI"
+sleep 1
+echo $cy"Thanks To : ALLAH SWT."
+sleep 1
+echo $me"Created By : TheKingTermux"
+sleep 1
+exit
+fi
