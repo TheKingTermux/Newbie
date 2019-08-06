bc116339ee490874369c7902a9e4ff6a45de7967
TermuxIna <50316075+TermuxIna@users.noreply.github.com>
2019-08-06 15:53:58 +0700

    Update password.sh

--- password.sh
+++ password.sh
@@ -1,72 +1,47 @@
-#!/bin/bash
+!/bin/sh
+figlet -f slant "LOGIN DLU YA!" |lolcat -a -d 10
+echo $cy'Masukkan Password Anda : '|lolcat -a -d 10
+read pw;
 
-figlet -f slant "LOGIN DLU" |lolcat -a -d 10
+if [ $pw = 'TKT' ];
+then
+echo 'Password anda benar' |lolcat -a -d 10
+echo 'Selamat datang di script ini' |lolcqt -a -d 10
+echo "[" $ij"Loading Script""]" |lolcat -a -d 10
+echo "[" $cy"=>                           "]" $bi"5%" |lolcat -a -d 10
+echo "[" $cy"==>                          "]" $bi"10%" |lolcat -a -d 10
+echo "[" $cy"===>                         "]" $bi"15%" |lolcat -a -d 10
+echo "[" $cy"====>                        "]" $bi"20%" |lolcat -a -d 10
+echo "[" $cy"=====>                       "]" $bi"25%" |lolcat -a -d 10
+echo "[" $cy"======>                      "]" $bi"30%" |lolcat -a -d 10
+echo "[" $cy"=======>                     "]" $bi"35%" |lolcat -a -d 10
+echo "[" $cy"========>                    "]" $bi"40%" |lolcat -a -d 10
+echo "[" $cy"=========>                   "]" $bi"45%" |lolcat -a -d 10
+echo "[" $cy"==========>                  "]" $bi"50%" |lolcat -a -d 10
+echo "[" $cy"===========>                 "]" $bi"55%" |lolcat -a -d 10
+echo "[" $cy"============>                "]" $bi"60%" |lolcat -a -d 10
+echo "[" $cy"=============>               "]" $bi"65%" |lolcat -a -d 10
+echo "[" $cy"==============>              "]" $bi"70%" |lolcat -a -d 10
+echo "[" $cy"===============>             "]" $bi"75%" |lolcat -a -d 10
+echo "[" $cy"================>            "]" $bi"80%" |lolcat -a -d 10
+echo "[" $cy"=================>           "]" $bi"84%" |lolcat -a -d 10
+echo "[" $cy"==================>          "]" $bi"88%" |lolcat -a -d 10
+echo "[" $cy"===================>         "]" $bi"91%" |lolcat -a -d 10
+echo "[" $cy"====================>        "]" $bi"92%" |lolcat -a -d 10
+echo "[" $cy"=====================>       "]" $bi"93%" |lolcat -a -d 10
+echo "[" $cy"======================>      "]" $bi"94%" |lolcat -a -d 10
+echo "[" $cy"=======================>     "]" $bi"95%" |lolcat -a -d 10
+echo "[" $cy"========================>    "]" $bi"96%" |lolcat -a -d 10
+echo "[" $cy"=========================>   "]" $bi"97%" |lolcat -a -d 10
+echo "[" $cy"==========================>  "]" $bi"98%" |lolcat -a -d 10
+echo "[" $cy"===========================> "]" $bi"99%" |lolcat -a -d 10
+echo "[" $cy"============================>"]" $bi"100%" |lolcat -a -d 10
 
-echo "["$cy"Mohon Masukkan Username Dan Password Demi Keamanan""]" |lolcat -a -d 10
+clear
+figlet -f slant "Login Sukses" |lolcat -a -d 10
 
-echo "["$cy"Bila Anda Kesusahan""]" |lolcat -a -d 10
-
-echo "["$cy"Silahkan Hubungi Pembuat Script""]" |lolcat -a -d 10
-
-username = 'TheKingTermux'
-
-password = 'TKT'
-
-
-
-def restart():
-
-        ngulang = sys.executable
-
-        os.execl(ngulang, ngulang, *sys.argv)
-
-
-
-def main():
-
-        uname = raw_input("username : ")
-
-        if uname == username:
-
-                pwd = raw_input("password : ")
-
-
-
-          if pwd == password:
-
-                        echo "Loading Masuk Ke Script"
-                        echo "Sabar Bosque"
-
-                        sys.exit()
-
-
-
-                else:
-
-                        echo "[" $cy"Password Atau Username Nya Salah Bosque""]"
-
-                        echo "["$cy"Silahkan Masukkan Username Yg Benar""]"
-
-                        restart()
-
-
-
-        else:
-
-                        echo "[" $cy"Password Atau Username Nya Salah Bosque""]"
-
-                        echo "["$cy"Silahkan Masukkan Username Yg Benar""]"
-
-                restart()
-
-
-
-try:
-
-        main()
-
-except KeyboardInterrupt:
-
-        os.system('clear')
-
-        restart()
+else
+figlet -f slant $cy"Login Gagal" |lolcat -a -d 10
+echo 'Password anda salah'
+echo 'Silakan Masukkan kembali'
+fi
