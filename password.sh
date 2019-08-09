b9620e7bf4954f39dadacac8ca2a1023469590ef
TermuxIna <50316075+TermuxIna@users.noreply.github.com>
2019-08-09 09:21:15 +0700

    Update password.sh

--- password.sh
+++ password.sh
@@ -21,7 +21,7 @@ echo $cy"FAILED"|lolcat -a -d 10
 sleep 2
 clear
 
-figlet -f slant $cy"LoginDlu Ya!" |lolcat -a -d 10
+figlet -f slant $cy"Login Dlu Ya!" |lolcat -a -d 10
 
 echo $cy"Sudah Punya Passwordnya? " |lolcat -a -d 10
 echo $cy"Klo Sudah Silahkan Login " |lolcat -a -d 10
@@ -54,35 +54,35 @@ figlet -f slant "MASUK SC" |lolcat -a -d 10
 echo ""
 echo "[" $ij"Loading Script""]" |lolcat -a -d 10
 echo ""
-echo "[" $cy"=>                           ""]" $bi"5%" |lolcat -a -d 10
-echo "[" $cy"==>                          ""]" $bi"10%" |lolcat -a -d 10
-echo "[" $cy"===>                         ""]" $bi"15%" |lolcat -a -d 10
-echo "[" $cy"====>                        ""]" $bi"20%" |lolcat -a -d 10
-echo "[" $cy"=====>                       ""]" $bi"25%" |lolcat -a -d 10
-echo "[" $cy"======>                      ""]" $bi"30%" |lolcat -a -d 10
-echo "[" $cy"=======>                     ""]" $bi"35%" |lolcat -a -d 10
-echo "[" $cy"========>                    ""]" $bi"40%" |lolcat -a -d 10
-echo "[" $cy"=========>                   ""]" $bi"45%" |lolcat -a -d 10
-echo "[" $cy"==========>                  ""]" $bi"50%" |lolcat -a -d 10
-echo "[" $cy"===========>                 ""]" $bi"55%" |lolcat -a -d 10
-echo "[" $cy"============>                ""]" $bi"60%" |lolcat -a -d 10
-echo "[" $cy"=============>               ""]" $bi"65%" |lolcat -a -d 10
-echo "[" $cy"==============>              ""]" $bi"70%" |lolcat -a -d 10
-echo "[" $cy"===============>             ""]" $bi"75%" |lolcat -a -d 10
-echo "[" $cy"================>            ""]" $bi"80%" |lolcat -a -d 10
-echo "[" $cy"=================>           ""]" $bi"84%" |lolcat -a -d 10
-echo "[" $cy"==================>          ""]" $bi"88%" |lolcat -a -d 10
-echo "[" $cy"===================>         ""]" $bi"91%" |lolcat -a -d 10
-echo "[" $cy"====================>        ""]" $bi"92%" |lolcat -a -d 10
-echo "[" $cy"=====================>       ""]" $bi"93%" |lolcat -a -d 10
-echo "[" $cy"======================>      ""]" $bi"94%" |lolcat -a -d 10
-echo "[" $cy"=======================>     ""]" $bi"95%" |lolcat -a -d 10
-echo "[" $cy"========================>    ""]" $bi"96%" |lolcat -a -d 10
-echo "[" $cy"=========================>   ""]" $bi"97%" |lolcat -a -d 10
-echo "[" $cy"==========================>  ""]" $bi"98%" |lolcat -a -d 10
-echo "[" $cy"===========================> ""]" $bi"99%" |lolcat -a -d 10
-echo "[" $cy"============================>""]" $bi"99.5%" |lolcat -a -d 10
-echo "[" $cy"=============================""]" $bi"100%" |lolcat -a -d 10
+echo "[" $cy"=>                           " "]" $bi"5%" |lolcat -a -d 10
+echo "[" $cy"==>                          " "]" $bi"10%" |lolcat -a -d 10
+echo "[" $cy"===>                         " "]" $bi"15%" |lolcat -a -d 10
+echo "[" $cy"====>                        " "]" $bi"20%" |lolcat -a -d 10
+echo "[" $cy"=====>                       " "]" $bi"25%" |lolcat -a -d 10
+echo "[" $cy"======>                      " "]" $bi"30%" |lolcat -a -d 10
+echo "[" $cy"=======>                     " "]" $bi"35%" |lolcat -a -d 10
+echo "[" $cy"========>                    " "]" $bi"40%" |lolcat -a -d 10
+echo "[" $cy"=========>                   " "]" $bi"45%" |lolcat -a -d 10
+echo "[" $cy"==========>                  " "]" $bi"50%" |lolcat -a -d 10
+echo "[" $cy"===========>                 " "]" $bi"55%" |lolcat -a -d 10
+echo "[" $cy"============>                " "]" $bi"60%" |lolcat -a -d 10
+echo "[" $cy"=============>               " "]" $bi"65%" |lolcat -a -d 10
+echo "[" $cy"==============>              " "]" $bi"70%" |lolcat -a -d 10
+echo "[" $cy"===============>             " "]" $bi"75%" |lolcat -a -d 10
+echo "[" $cy"================>            " "]" $bi"80%" |lolcat -a -d 10
+echo "[" $cy"=================>           " "]" $bi"84%" |lolcat -a -d 10
+echo "[" $cy"==================>          " "]" $bi"88%" |lolcat -a -d 10
+echo "[" $cy"===================>         " "]" $bi"91%" |lolcat -a -d 10
+echo "[" $cy"====================>        " "]" $bi"92%" |lolcat -a -d 10
+echo "[" $cy"=====================>       " "]" $bi"93%" |lolcat -a -d 10
+echo "[" $cy"======================>      " "]" $bi"94%" |lolcat -a -d 10
+echo "[" $cy"=======================>     " "]" $bi"95%" |lolcat -a -d 10
+echo "[" $cy"========================>    " "]" $bi"96%" |lolcat -a -d 10
+echo "[" $cy"=========================>   " "]" $bi"97%" |lolcat -a -d 10
+echo "[" $cy"==========================>  " "]" $bi"98%" |lolcat -a -d 10
+echo "[" $cy"===========================> " "]" $bi"99%" |lolcat -a -d 10
+echo "[" $cy"============================>" "]" $bi"99.5%" |lolcat -a -d 10
+echo "[" $cy"=============================" "]" $bi"100%" |lolcat -a -d 10
 clear
 figlet -f slant "Login Sukses" |lolcat -a -d 10
 echo ""
@@ -104,4 +104,5 @@ fi
       echo 'ERROR : Wrong Input....! '
       sleep 1
       echo $ulang
+
 done
