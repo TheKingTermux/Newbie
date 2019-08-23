fc0a69c1cb947264c0dd4d33b50be12158fa3751
TheKingTermux <50316075+TheKingTermux@users.noreply.github.com>
2019-08-23 07:52:55 +0700

    Update and rename README.md to baca dulu.sh

--- README.md
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-Assalamualaikum Wr. Wb.
-Halo guys perkanalkan nama saya achmad.
-Sya membuat script ini untuk kalian para pemula.
-Dengan adanya script ini semoga dapat membantu kalian para newbie newbie yg ingin hack 
-
-Itu saja.
-Wassalamualaikum Wr. Wb.
---
+++ baca dulu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#IniBuatParaNoobLiatIniDluOke
+#FreePasswordNyaLiatAjaNanti
+
+clear
+
+figlet -f slant  "Welcome" |lolcat -a -d 10
+echo ""
+echo "×××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××>
+echo "Assalamualaikum Wr. Wb."|lolcat -a -d 10
+echo "Halo guys perkanalkan nama saya achmad."|lolcat -a -d 10
+echo "Sya membuat script ini untuk kalian para pemula."|lolcat -a -d 10
+echo "Dengan adanya script ini semoga dapat membantu kalian para newbie newbie yg ingin menghack 
+echo "password untuk semua script nya adalah"  "TKT" "tanpa tanda petik"|lolcat -a -d 10
+echo 'Password nya adalah TKT'|lolcat -a -d 10
+echo 'Itu saja. Wassalamualaikum Wr. Wb.'|lolcat -a -d 10
+echo 'Ohhhh iya Jika kalian membutuhkan sya silahkan contac medsos sya'|lolcat -a -d 10
+echo "WhatsApp     : 087705736634"|lolcat -a -d 10
+echo "FaceBook     : TheKingg Termux"|lolcat -a -d 10
+echo "InstaGram    : @thekingtermux"|lolcat -a -d 10
+echo "Tweeter      : @achmad_arjosari"|lolcat -a -d 10
+echo "Email        : achmadmaulana0233@gmail.com"|lolcat -a -d 10
+echo "Jika kalian sudah memakai script ini silahkan kalian bantu share yaa!"|lolcat -a -d 10 >
+echo "Dengan sangad agar script ini berguna"|lolcat -a -d 10
+echo "Selamat memakai script pertama sya"|lolcat -a -d 10
+echo "Wassalamualaikum Wr. Wb"|lolcat -a -d 10
+echo "×××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××>
+sleep 2
+clear
